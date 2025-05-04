4324319015b9e34c56b603e674d220b49a56e3b4
80pi <gopivenkataajay5771@gmail.com>
2025-05-04 12:11:39 +0530

    changes related to file importing settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true,
+    port: 3001,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@resources": path.resolve(__dirname, "./src/assets/resources"),
+    },
+  },
+});
