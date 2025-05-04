2706ac79fc440c958a4dc5e4579a0f20d979a637
80pi <gopivenkataajay5771@gmail.com>
2025-05-04 12:12:24 +0530

    initial page setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import mockData from "@resources/data/data.json";
+
+const Header = () => {
+  const itemMockData = mockData;
+  console.log(mockData);
+  return <>Hasas</>;
+};
+
+export default Header;
