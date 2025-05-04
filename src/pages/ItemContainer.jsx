23847791b40f95b45c8507efe7a19a5d8e5994b9
80pi <gopivenkataajay5771@gmail.com>
2025-05-04 15:21:41 +0530

    screen 2a1 item description

---
+++ src/pages/ItemContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ItemView from "../components/Item/ItemView";
+import ItemDescriptionContainer from "./ItemDescriptionContainer";
+
+const ItemContainer = () => {
+  return (
+    <>
+      <ItemView />
+      <ItemDescriptionContainer />
+    </>
+  );
+};
+
+export default ItemContainer;
