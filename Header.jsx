9996421fe0ed3ccaf00e8a7bda78dee09835def8
80pi <gopivenkataajay5771@gmail.com>
2025-05-04 17:41:22 +0530

    adding logo for readme.md filr

---
+++ Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import favorite from "@icons/favorite.svg";
+import cart from "@icons/cart.svg";
+import factsSoft from "@icons/facts-soft.svg";
+import ImageIcon from "./ImageIcon";
+import "./Header.css";
+import { useMockData } from "../context/DataContext";
+import Badge from "@mui/material/Badge";
+
+const Header = () => {
+  const { mockData } = useMockData();
+  console.log(mockData);
+  return (
+    <div className="headerContainer">
+      <h4 className="headerTitle">{mockData?.article?.title}</h4>
+      <div className="headerIcons">
+        <ImageIcon imgPath={favorite} altText={"favorite-icon"} />
+        <ImageIcon imgPath={factsSoft} altText={"fact-soft-icon"} />
+        <Badge badgeContent={mockData?.cart?.items} color="error">
+          <ImageIcon imgPath={cart} altText={"cart-icon"} />
+        </Badge>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
