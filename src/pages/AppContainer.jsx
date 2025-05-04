abd075223a3d781bda49c658843fe9cb770f3b7c
80pi <gopivenkataajay5771@gmail.com>
2025-05-04 15:21:17 +0530

    changes realted to app styles

--- src/pages/AppContainer.jsx
+++ src/pages/AppContainer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
-import ItemPage from "./ItemPage";
+import ItemPage from "./ItemContainer";
 
 export const AppContainer = () => {
   return (
-    <>
+    <div style={{ margin: "-0.45rem" }}>
       <Header />
       <ItemPage />
-    </>
+    </div>
   );
 };
